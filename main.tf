b4dade80c4857c25ac1acd623f52d61411410fa9
ekanai <miu@jubilee.works>
2022-12-16 23:13:02 +0900

    first

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "aws_secretsmanager_secret" "secret" {
+  name       = var.secret_name
+  kms_key_id = var.kms_arn
+
+  tags = {
+    username = var.user_name
+  }
+}
+
+locals {
+  json = {
+    dbClusterIdentifier = var.cluster_name
+    engine              = "mysql"
+    host                = var.writer_endpoint
+    password            = var.password
+    port                = var.port
+    reader              = var.reader_endpoint
+    username            = var.user_name
+  }
+
+  masterarn_json = var.master_secret_arn == "" ? {} : { masterarn = var.master_secret_arn }
+}
+
+resource "aws_secretsmanager_secret_version" "version" {
+  secret_id     = aws_secretsmanager_secret.secret.id
+  secret_string = jsonencode(merge(local.json, local.masterarn_json))
+}
+
+resource "aws_secretsmanager_secret_rotation" "root" {
+  secret_id           = aws_secretsmanager_secret.secret.id
+  rotation_lambda_arn = var.rotator_arn
+
+  rotation_rules {
+    automatically_after_days = var.rotation_days
+  }
+}
+
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = aws_secretsmanager_secret.secret.id
+}
